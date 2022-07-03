d6b9a683b21d46bb250af62e8548ddc864ec634d
Shakyra Cornelius <shakyracornelius@Shakyras-MacBook-Air.local>
2022-07-03 04:14:05 -0400

    add avata

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,14 +1,23 @@
 AUTHOR = 'shak'
 SITENAME = 'Shakyra C. Cornelius'
 SITEURL = ''
+TIMEZONE = 'America/Antigua'
+DEFAULT_LANG = 'en'
 
 ABOUT_ME = 'Here to learn. Here to share'
-AVATAR = '/images/site_logo.png'
+AVATAR = 'extra/site_logo.jpg'
 
 # Paths
 PATH = 'content'
 PAGE_PATHS = ['pages']
 ARTICLE_PATHS = ['blog']
+STATIC_PATHS = ['images','extra']
+
+# Tell Pelican to change the path to 'static/custom.css' in the output dir
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/css/custom.css'},
+    'extra/custom.js': {'path': 'static/js/custom.js'}
+}
 
 # Top menus
 DISPLAY_CATEGORIES_ON_MENU = False
@@ -17,9 +26,7 @@ DISPLAY_ARTICLE_INFO_ON_INDEX = True
 
 SIDEBAR_ON_LEFT = True
 
-TIMEZONE = 'America/Antigua'
 
-DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -44,11 +51,14 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+# Theme, environments and plugins
 PLUGIN_PATHS = ['pelican-plugins']
 THEME = 'pelican-themes/pelican-bootstrap3'
 BOOTSTRAP_THEME = 'cyborg'
 PLUGIN_PATHS = ['/Users/shakyracornelius/Development/Pelican/virtualenvs/staticsite/shakyracc/pelican-plugins']
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 PLUGINS = ['i18n_subsites']
+CUSTOM_CSS = 'static/css/custom.css'
+CUSTOM_JS = 'static/js/custom.js'
 
 I18N_TEMPLATES_LANG = 'en'
